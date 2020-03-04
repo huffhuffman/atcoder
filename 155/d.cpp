b04401077dc06ef5822578292484fac5cc37eae0
huffhuffman <46079709+huffhuffman@users.noreply.github.com>
2020-03-05 00:43:29 +0900

    shoujin

--- 155/d.cpp
+++ 155/d.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 using ll = long long;
 using P = pair<ll, ll>;
-constexpr int INF = 1001001001;
-constexpr int MOD = 1000000007;
+constexpr ll INF = ll(1e18) + 1;  // 1e18 は doubleなので整数に直す
 
 int main() {
   cin.tie(nullptr);
@@ -18,7 +17,54 @@ int main() {
 
   sort(a.begin(), a.end());
 
-  
+  ll l = -INF;
+  ll r = INF;
+  while (l + 1 < r) {
+    ll x = (l + r) / 2; // 積
+    bool ok = [&] {
+      ll tot = 0;
+      for (int i = 0; i < n; i++) {
+        if(a[i] < 0) {
+          // 負
+          int l = -1, r = n;
+          while(l + 1 < r) {
+            int c = (l + r) / 2;
+            if(a[c] * a[i] < x) {
+              r = c;
+            } else {
+              l = c;
+            }
+          }
+          tot += n - r;
+        } else {
+          // 正
+          int l = -1, r = n;
+          while(l + 1 < r) {
+            int c = (l + r) / 2;
+            if(a[c] * a[i] < x) {
+              l = c;
+            } else {
+              r = c;
+            }
+          }
+          tot += r;
+        }
+        if(a[i] * a[i] < x) {
+          tot--; // 対角線の余計なカウントを消す
+        }
+      }
+      tot /= 2; // 斜め半分の余計なカウントを消す
+      return tot < k; // x未満な数の合計がk未満なやつの最大を探す
+    }();
+
+    if(ok) {
+      l = x;
+    } else {
+      r = x;
+    }
+  }
+
+  cout << l << endl;
 
   return 0;
 }
