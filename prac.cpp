fefa10d6aba70194f985ca03ca52cc6a12b21cae
huffhuffman <46079709+huffhuffman@users.noreply.github.com>
2020-04-28 23:33:15 +0900

    shoujin

--- prac.cpp
+++ prac.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
 using namespace std;
 using ll = long long;
 using P = pair<ll, ll>;
@@ -12,27 +12,10 @@ int main() {
   cin.tie(nullptr);
   ios::sync_with_stdio(false);
 
-  ll n, l;
-  cin >> n >> l;
-  string s;
-  cin >> s;
+  ll num;
+  cin >> num;
 
-  ll ans = 0;
-  ll curr = 1;
-  for (int i = 0; i < n; i++) {
-    if (s[i] == '+') {
-      curr++;
-    } else if (curr > 1) {
-      curr--;
-    }
-
-    if (curr > l) {
-      ans++;
-      curr = 1;
-    }
-  }
-
-  cout << ans << ln;
+  cout << num + num << ln;
 
   return 0;
 }
