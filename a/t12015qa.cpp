ac37974086413912b129a29e2678b0ccbb1af15d
huffhuffman <46079709+huffhuffman@users.noreply.github.com>
2020-04-30 22:05:16 +0900

    shoujin

---
+++ a/t12015qa.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+const string ln = "\n";
+constexpr int INF = 1001001001;
+constexpr int MOD = 1000000007;
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  cout << 654231 << ln;
+
+  return 0;
+}
