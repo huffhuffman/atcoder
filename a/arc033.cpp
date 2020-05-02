cb217278337ae84f911c5390184b4a5f675f225b
huffhuffman <46079709+huffhuffman@users.noreply.github.com>
2020-05-02 19:03:43 +0900

    shoujin

---
+++ a/arc033.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+const string ln = "\n";
+constexpr int INF = 1001001001;
+constexpr int MOD = 1000000007;
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+
+  ll ans = 0;
+  for (int i = 1; i <= n; i++) {
+    ans += i;
+  }
+
+  cout << ans << ln;
+
+  return 0;
+}
