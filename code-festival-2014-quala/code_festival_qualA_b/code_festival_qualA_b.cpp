2a34f1559082dad69ac02f3bb970f64ff2b5342e
huffhuffman <46079709+huffhuffman@users.noreply.github.com>
2020-06-08 22:59:23 +0900

    shoujin

---
+++ code-festival-2014-quala/code_festival_qualA_b/code_festival_qualA_b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+const string ln = "\n";
+constexpr int INF = 1001001001;
+constexpr int MOD = 1000000007;
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  string a;
+  ll b;
+  cin >> a >> b;
+
+  ll len = a.size();
+
+  ll i = (b - 1) % len;
+
+  cout << a[i] << ln;
+
+  return 0;
+}
