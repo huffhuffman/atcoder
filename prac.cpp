cded53edeb8fedb004d706372f557d62964aa7f0
huffhuffman <46079709+huffhuffman@users.noreply.github.com>
2020-07-10 22:25:09 +0900

    shoujin

--- prac.cpp
+++ prac.cpp
@@ -12,16 +12,39 @@ int main() {
   cin.tie(nullptr);
   ios::sync_with_stdio(false);
 
-  string s;
-  cin >> s;
-  ll len = s.size();
-  ll ans = 0;
-  for (int i = 0; i < len; i++) {
-    if (i % 2 == 0) {
-      ans += s[i] - '0';
-    } else {
-      ans -= s[i] - '0';
+  vector<ll> e(6), l(6);
+  ll b;
+  for (int i = 0; i < 6; i++) {
+    cin >> e[i];
+  }
+  cin >> b;
+  for (int i = 0; i < 6; i++) {
+    cin >> l[i];
+  }
+
+  ll cnt = 0;
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      if (l[i] == e[j]) {
+        cnt++;
+        break;
+      }
     }
+  }
+
+  ll ans = 0;
+  if (cnt == 6) {
+    ans = 1;
+  } else if (cnt == 5) {
+    if (count(all(l), b) > 0) {
+      ans = 2;
+    } else {
+      ans = 3;
+    }
+  } else if (cnt == 4) {
+    ans = 4;
+  } else if (cnt == 3) {
+    ans = 5;
   }
 
   cout << ans << ln;
