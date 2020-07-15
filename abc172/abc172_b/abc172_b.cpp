900cf0ad6de068d14710662ebefef3020b7e830b
huffhuffman <46079709+huffhuffman@users.noreply.github.com>
2020-07-15 22:46:01 +0900

    shoujin

---
+++ abc172/abc172_b/abc172_b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+const string ln = "\n";
+constexpr int INF = 1001001001;
+constexpr int MOD = 1000000007;
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  string s, t;
+  cin >> s >> t;
+
+  ll len = s.size();
+  ll ans = 0;
+  for (int i = 0; i < len; i++) {
+    if (s[i] != t[i]) {
+      ans++;
+    }
+  }
+
+  cout << ans << ln;
+
+  return 0;
+}
