0e54c9a34055e47ae6bb19d4493306e974a50eee
huffhuffman <46079709+huffhuffman@users.noreply.github.com>
2020-07-19 21:56:31 +0900

    shoujin

---
+++ aising2020/aising2020_b/aising2020_b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+const string ln = "\n";
+constexpr int INF = 1001001001;
+constexpr int MOD = 1000000007;
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+
+  vector<ll> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  ll ans = 0;
+  for (int i = 0; i < n; i++) {
+    if (i % 2 == 0 && a[i] % 2 == 1) {
+      ans++;
+    }
+  }
+
+  cout << ans << ln;
+
+  return 0;
+}
